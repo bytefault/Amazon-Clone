348df21ab4d3eeeb1f3d6adf9d473a1da31543df
jyoti <jyotivish0806@gmail.com>
2024-10-15 16:50:03 +0530

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,11 +1,15 @@
 import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
-import { products } from "../data/products.js";
+import { products, loadProducts } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-let productsHTML = ""; // to store html of each product
+//loadproducts ko response laane me time lagta utne me neeche kaa saara code run ho jaata without produtcs, aur page blank rehta, lekin ham chahte hai ki ek baar load ho jaaye firee ye runn kare, to funtion bana diya aur as a parameter pass kar diya, baad me run karne ke liye
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
+function renderProductsGrid() {
+  let productsHTML = ""; // to store html of each product
+
+  products.forEach((product) => {
+    productsHTML += `
         <div class="product-container">
             <div class="product-image-container">
                 <img
@@ -57,24 +61,24 @@ products.forEach((product) => {
               product.id
             }">Add to Cart</button>
             </div>`;
-});
+  });
 
-document.querySelector(".js-products-grid").innerHTML = productsHTML; //update the inner html on the main page
+  document.querySelector(".js-products-grid").innerHTML = productsHTML; //update the inner html on the main page
 
-// update the cart quantity on the main page
-function updateCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
-  if (cartQuantity !== 0) {
-    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+  // update the cart quantity on the main page
+  function updateCartQuantity() {
+    const cartQuantity = calculateCartQuantity();
+    if (cartQuantity !== 0) {
+      document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    }
   }
-}
-updateCartQuantity();
+  updateCartQuantity();
 
-//const addedMessageTimeouts = {}; // To store timeout IDs for each product
+  //const addedMessageTimeouts = {}; // To store timeout IDs for each product
 
-//changes on cliking add to cart button
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  /*This solution uses a feature of JavaScript called a
+  //changes on cliking add to cart button
+  document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+    /*This solution uses a feature of JavaScript called a
     closure. Each time we run the loop, it will create 
     a new variable called addedMessageTimeoutId and do
     button.addEventListener().
@@ -91,37 +95,37 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     addedMessageTimeoutId variable (one for every time
     we run the loop) so it lets us keep track of many
     timeoutIds (one for each product). */
-  let addedMessageTimeoutId;
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId; //get the product ID
+    let addedMessageTimeoutId;
+    button.addEventListener("click", () => {
+      const productId = button.dataset.productId; //get the product ID
 
-    const quantitySelector = document.querySelector(
-      `.js-quantity-selector-${productId}`
-    );
-    const quantity = Number(quantitySelector.value); //get the quantity you select for a product
+      const quantitySelector = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const quantity = Number(quantitySelector.value); //get the quantity you select for a product
 
-    addToCart(productId, quantity); //logic to add product to cart
-    updateCartQuantity(); //update cart UI
+      addToCart(productId, quantity); //logic to add product to cart
+      updateCartQuantity(); //update cart UI
 
-    const addedMessage = document.querySelector(
-      `.js-added-to-cart-${productId}`
-    );
-    addedMessage.classList.add("added-to-cart-visible"); //show the message
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+      addedMessage.classList.add("added-to-cart-visible"); //show the message
 
-    // Check if a previous timeoutId exists. If it does,
-    // we will stop it.
-    if (addedMessageTimeoutId) {
-      clearTimeout(addedMessageTimeoutId);
-    }
+      // Check if a previous timeoutId exists. If it does,
+      // we will stop it.
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
 
-    const timeoutId = setTimeout(() => {
-      addedMessage.classList.remove("added-to-cart-visible");
-    }, 2000);
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove("added-to-cart-visible");
+      }, 2000);
 
-    // Save the timeoutId so we can stop it later.
-    addedMessageTimeoutId = timeoutId;
+      // Save the timeoutId so we can stop it later.
+      addedMessageTimeoutId = timeoutId;
 
-    /* // If a previous timeout exists, cancel it
+      /* // If a previous timeout exists, cancel it
     const previousTimeoutId = addedMessageTimeouts[productId];
     if (previousTimeoutId) {
       clearTimeout(previousTimeoutId);
@@ -135,8 +139,9 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     // Store the new timeout ID
     addedMessageTimeouts[productId] = timeoutId; */
 
-    // setTimeout(() => {
-    //   addedMessage.classList.remove("added-to-cart-visible");
-    // }, 2000);
+      // setTimeout(() => {
+      //   addedMessage.classList.remove("added-to-cart-visible");
+      // }, 2000);
+    });
   });
-});
+}
